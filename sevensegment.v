5593aa8effca55177bc828e55c1c3e1e384c9a5f
Mary Hailu <mary89@vt.edu>
2018-01-04 15:24:07 -0800

    Update sevensegment.v

--- sevensegment.v
+++ sevensegment.v
@@ -13,14 +13,14 @@ module seven_segment(in, out);											// module start here
 		 always @ (in) begin												   // always block begin here
             case(in)
 	 
-                  4'b0000: out = 7'b1000000;							
+                			        4'b0000: out = 7'b1000000;							
 						4'b0001: out = 7'b1111001;
 						4'b0010: out = 7'b0100100;
 						4'b0011: out = 7'b0110000;
 						4'b0100: out = 7'b0011001;
-                  4'b0101: out = 7'b0010010;
+                  				4'b0101: out = 7'b0010010;
 						4'b0110: out = 7'b0000010;
-                  4'b0111: out = 7'b1111000;
+                 				4'b0111: out = 7'b1111000;
 						4'b1000: out = 7'b0000000;
 						4'b1001: out = 7'b0010000;
 						4'b1010: out = 7'b0001000; 
